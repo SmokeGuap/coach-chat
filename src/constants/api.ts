d0d6df79bc5f376dc514e1a237f0de83004e2fe7
Kirill <mustang.horse34@gmail.com>
2024-02-20 14:54:54 +0700

    add constants

---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const API_URL = 'http://176.99.11.136/api/';
+export const API_URL_WEBSOCKET = 'ws://176.99.11.136/ws/room/';
---
+++ src/constants/paths.ts
@@ -0,0 +1,11 @@
+export enum Paths {
+  //Guest routes
+  home = '/',
+  signInUser = '/sign-in-user',
+  signInCoach = '/sign-in-coach',
+
+  //Private routes
+  createRoom = '/create-room',
+  room = '/room',
+  chatClose = '/chat-close',
+}
