f541d2ce619084c12d4c906c6f3630e4658fa704
Kirill <mustang.horse34@gmail.com>
2024-02-20 14:55:12 +0700

    add hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useLogOut } from './useLogOut';
+export { default as useWindowWidth } from './useWindowWidth';
+export { default as useAppDispatch } from './useAppDispatch';
+export { default as useAppSelector } from './useAppSelector';
